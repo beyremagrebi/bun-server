67c1bebd0056088922c2a8dcd1a246982fe1063d
ag-beyrem <beyrem.agrebi@proservices-training-company.com>
2025-05-23 11:37:51 +0100

    fix identifier

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -7,6 +7,8 @@ import { Get, Post } from "../routes/router-manager";
 import { generateToken } from "../utils/j-w-t";
 import { ResponseHelper } from "../utils/response-helper";
 import { BaseController } from "./base/base-controller";
+import { authMiddleware } from "../middleware/aut-middleware";
+import validator from "validator";
 
 class AuthController extends BaseController<RefreshToken> {
   constructor() {
@@ -22,16 +24,23 @@ class AuthController extends BaseController<RefreshToken> {
       const body =
         await this.parseRequestBody<OptionalUnlessRequiredId<User>>(req);
 
-      if (!body.email && !body.userName) {
+      if (!body.identifier) {
         return ResponseHelper.error("Email or username is required", 400);
       }
 
-      const user = await CollectionsManager.userCollection.findOne({
-        $or: [
-          { email: body.email ?? null },
-          { userName: body.userName ?? null },
-        ],
-      });
+      let isEmail = false;
+      if (validator.isEmail(body.identifier)) {
+        isEmail = true;
+      }
+      const query: { email?: string; userName?: string } = {};
+
+      if (isEmail) {
+        query.email = body.identifier;
+      } else {
+        query.userName = body.identifier;
+      }
+
+      const user = await CollectionsManager.userCollection.findOne(query);
 
       if (!user) {
         return ResponseHelper.error("User not found", 404);
@@ -51,7 +60,7 @@ class AuthController extends BaseController<RefreshToken> {
     }
   }
 
-  @Post("/logout")
+  @Post("/logout", [authMiddleware])
   async logout(req: ServerRequest): Promise<Response> {
     try {
       return ResponseHelper.success(req);
--- src/models/user.ts
+++ src/models/user.ts
@@ -3,6 +3,7 @@ import type { BaseModel } from "./base/base-model";
 export interface User extends BaseModel {
   firstName: string;
   lastName: string;
+  identifier?: string;
   userName: string;
   email: string;
   password: string;
